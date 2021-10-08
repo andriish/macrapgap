9c8ba913a184fdfe7657649ee6b667f7fc244e88
Alois Kabelschacht <andrii.verbytskyi@mpp.mpg.de>
2021-10-08 22:38:41 +0200

    OK

--- .github/workflows/osx/entrypoint1.sh
+++ .github/workflows/osx/entrypoint1.sh
@@ -80,7 +80,7 @@ rm -rf libtool configure aclocal.m4
 #AUTOTOOLS MUST DIE
 #https://stackoverflow.com/questions/53121019/ld-bind-at-load-and-bitcode-bundle-xcode-setting-enable-bitcode-yes-cannot
 autoreconf -isv 
-export MACOSX_DEPLOYMENT_TARGET-10.0="10.15"
+export MACOSX_DEPLOYMENT_TARGET=10.15
 #./configure  --disable-shared --prefix=$(pwd)/TESTINSTALLDIR --with-hepmc2=/usr/local --with-hepmc3=no --with-lhapdf6=/usr/local
 ./configure --with-pic  --disable-static --prefix=$(pwd)/TESTINSTALLDIR --with-hepmc2=/usr/local --with-hepmc3=no --with-lhapdf6=/usr/local
 
