2f27329568b521dd08ed6828ef51e00c2c788de0
Alois Kabelschacht <andrii.verbytskyi@mpp.mpg.de>
2021-10-08 23:37:33 +0200

    OK

--- .github/workflows/osx/entrypoint1.sh
+++ .github/workflows/osx/entrypoint1.sh
@@ -82,19 +82,19 @@ rm -rf libtool configure aclocal.m4
 autoreconf -fisv 
 export MACOSX_DEPLOYMENT_TARGET=11.5
 #./configure  --disable-shared --prefix=$(pwd)/TESTINSTALLDIR --with-hepmc2=/usr/local --with-hepmc3=no --with-lhapdf6=/usr/local
-./configure --with-pic  --disable-static --prefix=$(pwd)/TESTINSTALLDIR --with-hepmc2=/usr/local --with-hepmc3=no --with-lhapdf6=/usr/local
+./configure   --disable-static --prefix=$(pwd)/TESTINSTALLDIR --with-hepmc2=/usr/local --with-hepmc3=no --with-lhapdf6=/usr/local
 
-cat libtool | grep -C 10 MACOSX_DEPLOYMENT_TARGET 
+#cat libtool | grep -C 10 MACOSX_DEPLOYMENT_TARGET 
 #gsed -i 's/\$wl-bind_at_load//g' libtool
 #gsed -i 's/no-common/PIC/g' libtool
 #SO FAR DW wo below
 #gsed -i 's/dynamic_lookup/dynamic_lookup \\\$\{wl\}\-flat_namespace/g' libtool
 
 
-cat libtool | grep -C 10 MACOSX_DEPLOYMENT_TARGET 
-cat libtool | grep -C 10 bind_at_load 
-cat libtool | grep -C 10 '\$wl'
-cat libtool | grep -C 10 'PIC'
+#cat libtool | grep -C 10 MACOSX_DEPLOYMENT_TARGET 
+#cat libtool | grep -C 10 bind_at_load 
+#cat libtool | grep -C 10 '\$wl'
+#cat libtool | grep -C 10 'PIC'
 make -j 2 
 make install 
 export DYLD_PRINT_LIBRARIES=1
