c61f1ffc3b48ddb1e51b0fb5f2a19e8be9511b41
Alois Kabelschacht <andrii.verbytskyi@mpp.mpg.de>
2021-10-08 22:29:52 +0200

    OK

--- .github/workflows/osx/entrypoint1.sh
+++ .github/workflows/osx/entrypoint1.sh
@@ -80,12 +80,12 @@ rm -rf libtool configure aclocal.m4
 #AUTOTOOLS MUST DIE
 #https://stackoverflow.com/questions/53121019/ld-bind-at-load-and-bitcode-bundle-xcode-setting-enable-bitcode-yes-cannot
 autoreconf -isv 
-#export MACOSX_DEPLOYMENT_TARGET-10.0="10.14.1"
+export MACOSX_DEPLOYMENT_TARGET-10.0="10.15"
 #./configure  --disable-shared --prefix=$(pwd)/TESTINSTALLDIR --with-hepmc2=/usr/local --with-hepmc3=no --with-lhapdf6=/usr/local
 ./configure --with-pic  --disable-static --prefix=$(pwd)/TESTINSTALLDIR --with-hepmc2=/usr/local --with-hepmc3=no --with-lhapdf6=/usr/local
 
 cat libtool | grep -C 10 MACOSX_DEPLOYMENT_TARGET 
-gsed -i 's/\$wl-bind_at_load//g' libtool
+#gsed -i 's/\$wl-bind_at_load//g' libtool
 #gsed -i 's/no-common/PIC/g' libtool
 gsed -i 's/dynamic_lookup/dynamic_lookup \\\$\{wl\}\-flat_namespace/g' libtool
 
